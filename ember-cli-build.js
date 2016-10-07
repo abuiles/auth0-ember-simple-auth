f2ee280531b8422acb3e852939603aefcc452333
Adolfo Builes <builes.adolfo@gmail.com>
2016-10-06 22:54:25 -0500

    Ignore lock and friends if running fastboot.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -11,9 +11,11 @@ module.exports = function(defaults) {
     addon, located in `/tests/dummy`
     This build file does *not* influence how the addon or the app using it
     behave. You most likely want to be modifying `./index.js` or app's build file
-  */
-  app.import(app.bowerDirectory + '/auth0-lock/build/auth0-lock.js');
-  app.import(app.bowerDirectory + '/jsrsasign/jsrsasign-latest-all-min.js');
+   */
+  if (!process.env.EMBER_CLI_FASTBOOT) {
+    app.import(app.bowerDirectory + '/auth0-lock/build/auth0-lock.js');
+    app.import(app.bowerDirectory + '/jsrsasign/jsrsasign-latest-all-min.js');
+  }
 
   return app.toTree();
 };
--- index.js
+++ index.js
@@ -5,7 +5,9 @@ module.exports = {
   name: 'auth0-ember-simple-auth',
 
   included: function(app) {
-    app.import(app.bowerDirectory + '/auth0-lock/build/auth0-lock.js');
-    app.import(app.bowerDirectory + '/jsrsasign/jsrsasign-latest-all-min.js');
+    if (!process.env.EMBER_CLI_FASTBOOT) {
+      app.import(app.bowerDirectory + '/auth0-lock/build/auth0-lock.js');
+      app.import(app.bowerDirectory + '/jsrsasign/jsrsasign-latest-all-min.js');
+    }
   }
 };
